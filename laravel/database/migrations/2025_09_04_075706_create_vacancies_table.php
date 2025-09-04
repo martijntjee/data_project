5fd6c8679c416126ba6a4356d74fd89b3fa2fdc4
MartijnKerpentier <162339@student.horizoncollege.nl>
2025-09-04 10:45:18 +0200

    upload mirgrations

---
+++ laravel/database/migrations/2025_09_04_075706_create_vacancies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vacancies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('company');
+            $table->string('location');
+            $table->string('category');
+            $table->integer('salary_min')->nullable();
+            $table->integer('salary_max')->nullable();
+            $table->text('description');
+            $table->foreignId('profile_id')->constrained('profiles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vacancies');
+    }
+};
