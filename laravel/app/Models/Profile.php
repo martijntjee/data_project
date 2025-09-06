504daa37e4eebc103b2d1da9f0ed192515cd1f49
MartijnKerpentier <162339@student.horizoncollege.nl>
2025-09-06 13:04:39 +0200

    models able to use factory

--- laravel/app/Models/Profile.php
+++ laravel/app/Models/Profile.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Profile extends Model
 {
+    use HasFactory;
+
     public function vacancies()
     {
         return $this->hasMany(Vacancy::class);
