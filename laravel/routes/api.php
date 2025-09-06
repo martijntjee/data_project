5830a3bca6afd4f0f0057bd559d282e2a22ef8a3
MartijnKerpentier <162339@student.horizoncollege.nl>
2025-09-06 19:48:39 +0200

    upload create function

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\VacancyController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/vacancies', [VacancyController::class, 'store']);
 Route::get('/vacancies', [VacancyController::class, 'index']);
 Route::get('/vacancies/{id}', [VacancyController::class, 'show']);
